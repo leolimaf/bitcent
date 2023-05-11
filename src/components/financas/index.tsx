f4f01166507678b2135d7624d33bb3d3a754338a
Leonardo Lima <leonardolimaf54@gmail.com>
2023-05-11 20:25:59 -0300

    finanças: lista de transações

--- src/components/financas/index.tsx
+++ src/components/financas/index.tsx
@@ -1,13 +1,19 @@
+import transacoesFalsas from "@/data/constants/transacoesFalsas";
 import Cabecalho from "../template/Cabecalho";
 import Conteudo from "../template/Conteudo";
 import Pagina from "../template/Pagina";
+import { useState } from "react";
+import Transacao from "@/logic/core/financas/Transacao";
+import Lista from "./Lista";
 
 export default function Financas() {
+    const [transacoes, setTransacoes] = useState<Transacao[]>(transacoesFalsas)
+
     return (
         <Pagina>
             <Cabecalho />
             <Conteudo>
-                conteudo
+                <Lista transacoes={transacoes} />
             </Conteudo>
         </Pagina>
         )
